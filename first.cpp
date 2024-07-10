97592022cdea427d5780de40bd4059e99d5a54b8
NishantSinghhh <143862018+NishantSinghhh@users.noreply.github.com>
2024-07-10 09:43:30 +0530

    Update first.cpp
    I have added variation in the c++ function so had to change the whole code that's why changing the whole repo

--- first.cpp
+++ first.cpp
@@ -1,125 +1,155 @@
-# include<iostream>
+#include <iostream>
+#include <cmath> // For std::sqrt, std::atan2
+
 using namespace std;
 
-//decaring Class Complex
-class Complex{
- double real;     // declaring real part of the complext number  
- double img;        // declaring imaginary part of the real number
- public: 
- Complex();            // Default Constructor
- friend istream & operator >> (istream &, Complex &); // Input 
- friend ostream & operator << (ostream &, const Complex &); // Output
+class Complex {
+private:
+    double real;
+    double img;
 
- Complex operator + (Complex); // Addition
- Complex operator * (Complex); // Multiplication
+public:
+    Complex() : real(0.0), img(0.0) {}
+    Complex(double r, double i) : real(r), img(i) {}
 
- };
-
- // Default Constructor 
-
- Complex::Complex(){
- real = 0;
- img = 0;
- }
-
-//   function to take input
- istream & operator >> (istream &, Complex & i){
- cin >> i.real >> i.img;
- return cin;
- }
-
-//  function to take output
- ostream & operator << (ostream &, const Complex & d){
- cout << d.real << " + " << d.img << "i" << endl;
- return cout;
- }
-
-// Overloading + operator
- Complex Complex::operator + (Complex c1){
- Complex temp;
- temp.real = real + c1.real;
- temp.img = img + c1.img;
- return temp;
- }
-
-// Overloading * Operator
- Complex Complex::operator * (Complex c2){
- Complex tmp;
- tmp.real = real * c2.real - img * c2.img;
- tmp.img = real * c2.img + img * c2.real;
- return tmp;
- }
-
-
- int main(){
-
- Complex C1, C2, C3, C4;
- int flag = 1;
- char b;
- while (flag == 1){
- cout << "Enter Real and Imaginary part of the Complex Number 1 : \n";
- cin >> C1;
- cout << "Enter Real and Imaginary part of the Complex Number 2 : \n";
- cin >> C2;
- int f = 1;
-
- while (f == 1){
- cout << "Complex Number 1 : " << C1 << endl;
- cout << "Complex Number 2 : " << C2 << endl;
-
- cout << "**********MENU**********" << endl;
- cout << "1. Addition of Complex Numbers" << endl;
- cout << "2. Multiplication of Complex Numbers" << endl;
- cout << "3. Exit\n";
-
- int a;
- 
- cout << "Enter your choice from above MENU (1 to 3) : ";
- cin >> a;
-
- if (a == 1){
-
- C3 = C1+C2;
- cout << "Addition : " << C3 << endl;
- cout << "Do you wan to perform another operation (y/n) : \n";
- cin >> b;
-
- if (b == 'y' || b == 'Y'){
-
- f=1;
-
- }
- else{
-
- cout << "Thanks for using this program!!\n";
- flag=0;
- f=0;
-
- }
-
- }
- else if (a == 2){
-
- C4 = C1 * C2;
- cout << "Multiplication : " << C4 << endl;
- cout << "Do you wan to perform another operation (y/n) : \n";
- cin >> b;
-
- if (b == 'y' || b == 'Y'){
-            f=1;
- }
- else{
-    cout << "Thanks for using this program!!\n";
-        flag=0;
-            f=0;
+    Complex operator+(const Complex& other) const {
+        return Complex(real + other.real, img + other.img);
     }
- }
- else{
-    cout << "Thanks for using this program!!\n";
-        flag=0;
-            f=0;
+
+    Complex operator-(const Complex& other) const {
+        return Complex(real - other.real, img - other.img);
+    }
+
+    Complex operator*(const Complex& other) const {
+        return Complex(real * other.real - img * other.img,
+                       real * other.img + img * other.real);
+    }
+
+    Complex operator/(const Complex& other) const {
+        double denominator = other.real * other.real + other.img * other.img;
+        if (denominator == 0) {
+            throw invalid_argument("Division by zero");
         }
+        return Complex((real * other.real + img * other.img) / denominator,
+                       (img * other.real - real * other.img) / denominator);
     }
- }
+    
+    // Absolute value (magnitude) of the complex number
+    double abs() const {
+        return sqrt(real * real + img * img);
+    }
+    
+    // Conjugate of the complex number
+    Complex conjugate() const {
+        return Complex(real, -img);
+    }
+    
+    // Sin function for complex number
+    Complex sin() const {
+        return Complex(std::sin(real) * std::cosh(img), std::cos(real) * std::sinh(img));
+    }
+
+    // Cos function for complex number
+    Complex cos() const {
+        return Complex(std::cos(real) * std::cosh(img), -std::sin(real) * std::sinh(img));
+    }
+
+    // Tan function for complex number
+    Complex tan() const {
+        return sin() / cos();
+    }
+
+    // Output operator
+    friend ostream& operator<<(ostream& out, const Complex& c) {
+        out << c.real << " + " << c.img << "i";
+        return out;
+    }
+
+    // Input operator
+    friend istream& operator>>(istream& in, Complex& c) {
+        cout << "Enter real part: ";
+        in >> c.real;
+        cout << "Enter imaginary part: ";
+        in >> c.img;
+        return in;
+    }
+};
+
+int main() {
+    Complex C1, C2, result;
+    int choice;
+
+    do {
+        cout << "Enter Real and Imaginary part of Complex Number 1:\n";
+        cin >> C1;
+        cout << "Enter Real and Imaginary part of Complex Number 2:\n";
+        cin >> C2;
+
+        cout << "********** MENU **********" << endl;
+        cout << "1. Addition" << endl;
+        cout << "2. Subtraction" << endl;
+        cout << "3. Multiplication" << endl;
+        cout << "4. Division" << endl;
+        cout << "5. Absolute Value" << endl;
+        cout << "6. Conjugate" << endl;
+        cout << "7. Sin" << endl;
+        cout << "8. Cos" << endl;
+        cout << "9. Tan" << endl;
+        cout << "10. Exit" << endl;
+
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                result = C1 + C2;
+                cout << "Result of Addition: " << result << endl;
+                break;
+            case 2:
+                result = C1 - C2;
+                cout << "Result of Subtraction: " << result << endl;
+                break;
+            case 3:
+                result = C1 * C2;
+                cout << "Result of Multiplication: " << result << endl;
+                break;
+            case 4:
+                try {
+                    result = C1 / C2;
+                    cout << "Result of Division: " << result << endl;
+                } catch (const std::invalid_argument& e) {
+                    cout << "Error: " << e.what() << endl;
+                }
+                break;
+            case 5:
+                cout << "Absolute Value of Complex Number 1: " << C1.abs() << endl;
+                cout << "Absolute Value of Complex Number 2: " << C2.abs() << endl;
+                break;
+            case 6:
+                cout << "Conjugate of Complex Number 1: " << C1.conjugate() << endl;
+                cout << "Conjugate of Complex Number 2: " << C2.conjugate() << endl;
+                break;
+            case 7:
+                cout << "Sine of Complex Number 1: " << C1.sin() << endl;
+                cout << "Sine of Complex Number 2: " << C2.sin() << endl;
+                break;
+            case 8:
+                cout << "Cosine of Complex Number 1: " << C1.cos() << endl;
+                cout << "Cosine of Complex Number 2: " << C2.cos() << endl;
+                break;
+            case 9:
+                cout << "Tangent of Complex Number 1: " << C1.tan() << endl;
+                cout << "Tangent of Complex Number 2: " << C2.tan() << endl;
+                break;
+            case 10:
+                cout << "Exiting the program. Thank you!\n";
+                break;
+            default:
+                cout << "Invalid choice! Please enter a number between 1 and 10.\n";
+        }
+
+        cout << "------------------------\n";
+    } while (choice != 10);
+
     return 0;
- }
+}
