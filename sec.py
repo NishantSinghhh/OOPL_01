6ce961e7db972df03d40d841385b1fe06370700a
NishantSinghhh <143862018+NishantSinghhh@users.noreply.github.com>
2024-07-10 14:16:59 +0530

    Update sec.py

--- sec.py
+++ sec.py
@@ -75,3 +75,67 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+#  Second assignment
+n = int(input("Enter the total Number of students in the class: "))
+cric_count = int(input("Enter the number of students that like to play cricket"))
+badi_count = int(input("Enter the number of students who play football"))
+foot_count = int(input("Enter the number of students that like to play cricket"))
+
+cr_list = []
+badi_list = []
+foot_list = []
+
+for i in range(0, cric_count)
+    user_input = input("Enter the roll number of students who play cricket : ")
+    cr_list.append(user_input)
+
+for i in range(0, badi_count)
+    user_input = input("Enter the roll number of students who play Badminton : ")
+    badi_list.append(user_input)
+
+for i in range(0, foot_count)
+    user_input = input("Enter the roll number of students who play Football : ")
+   foot_list.append(user_input)
+
+
+x1 = []
+x2 = []
+x3 = []
+x4 = []
+x5 = []
+x6 = []
+x7 = []
+
+for roll_number in range(1, n+1):
+    if str(roll_number) in cr_list and str(roll_number) in badi_list:
+        x1.append(str(roll_number))
+
+    if str(roll_number) in cr_list and str(roll_number) in badi_list:
+        if str(roll_number) not in foot_list:
+            x2.append(str(roll_number))
+
+    if str(roll_number) not in cr_list and str(roll_number) not in badi_list:
+        x3.append(str(roll_number))
+
+    if str(roll_number)  in cr_list and str(roll_number) in badi_list and str(roll_number) not in foot_list:
+        x4.append(str(roll_number))
+
+    if str(roll_number) not in cr_list and str(roll_number) not in badi_list and str(roll_number) not in foot_list:
+        x5.append(str(roll_number))    
+
+    if str(roll_number) in cr_list or str(roll_number) in badi_list or str(roll_number) in foot_list:
+        x6.append(str(roll_number))        
+
+    if str(roll_number) in cr_list and str(roll_number) in badi_list and str(roll_number) in foot_list:
+        x7.append(str(roll_number))      
+
+
+
+print("Students who play both cricket and badminton: ", x1)
+print("Students who play either cricket or badminton but not football : ", x2)   
+print("Students who play neither badminton nor cricket: ", x3)   
+print("Students who play both cricket and football but not badminton : ", x4) 
+print("Students who do not play any game: ", x5)
+print("Students who play at least one game : ", x6)       
+print("Students who play all the games : ", x7)   
